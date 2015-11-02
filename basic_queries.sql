5fa45a4256ac8fd905fa838c4a42dd7f58c41dc3
MiaKoda Grodsky <mcgrodsky@gmail.com>
2015-11-02 16:23:16 -0500

    Update basic_queries.sql

--- basic_queries.sql
+++ basic_queries.sql
@@ -3,27 +3,32 @@
 
 
 -- Get all information about all authors
-
+SELECT * FROM authors;
 -- Get just the name and birth year of all authors
 SELECT name, birth_year FROM authors;
 
 -- Get all authors born in the 20th centruy or later
-
+SELECT * FROM authors WHERE birth_year >= 1900;
 -- Get all authors born in the USA
-SELECT * FROM authors WHERE  nationality = 'United States of America';
+SELECT * FROM authors WHERE nationality = 'United States of America';
 -- Get all books published on 1985
 
+--Get all books
+SELECT * FROM books;
 -- Get all books published before 1989
-
+SELECT FROM books WHERE publication_date < 1989;
 -- Get just the title of all books.
+SELECT title FROM books;
 
 -- Get just the year that 'A Dance with Dragons' was published
   -- Cry when you realize how long it's been
-
+  SELECT publication_date FROM books WHERE title = 'A Dance with Dragons';
 -- Get all books which have `the` somewhere in their title (hint, look up LIKE/ILIKE)
-
+SELECT title FROM books WHERE title LIKE '%the%';
+or
+SELECT title FROM books WHERE title iLIKE '%the%'; <selects all with The as the title
 -- Add yourself as an author
-
+INSERT INTO authors(name, nationality, birth_year) VALUES ('Mia Grodsky', 'United States of America', 1992);
 -- Add two books that you'd like to write (you can hard-code your id as the author id)
 
 -- Update one of your books to have a new title
